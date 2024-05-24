59f7f903c68dcdd4d9b1e0f1e2ba8315fcf5f85d
Luciano <168549145+Luciano3L@users.noreply.github.com>
2024-05-24 20:57:16 -0300

    Update desafio.js para Calcular partidas Rankeadas

--- desafio.js
+++ desafio.js
@@ -1,21 +1,32 @@
-let nome = '"Zelda"';
-let nivel = 'Ferro';
-let xp = 1500;
+function calculoRank(vitorias, derrotas) {
 
-if (xp < 1000) {
-  nivel = 'Ferro';
-} else if (xp < 2000) {
-  nivel = 'Bronze';
-} else if (xp < 5000) {
-  nivel = 'Prata';
-} else if (xp < 7000) {
-  nivel = 'Ouro';
-} else if (xp < 8000) {
-  nivel = 'Platina';
-} else if (xp < 9000) {
-  nivel = 'Ascendente';
-} else {
-  nivel = 'Imortal';
+  saldoVitorias = vitorias - derrotas;
+
+  let nivel;
+
+  if (saldoVitorias < 10)
+    nivel = "Ferro";
+    else if (saldoVitorias >= 11 && saldoVitorias <= 20)
+    nivel = "Bronze";
+    else if (saldoVitorias >= 21 && saldoVitorias <= 50)
+    nivel = "Prata";
+    else if (saldoVitorias >= 51 && saldoVitorias <= 80)
+    nivel = "Ouro";
+    else if (saldoVitorias >= 81 && saldoVitorias <= 90)
+    nivel = "Diamante";
+    else if (saldoVitorias >= 91 && saldoVitorias <= 100)
+    nivel = "Lendário";
+    else 
+    nivel = "Imortal";
+
+
+  return { saldoVitorias, nivel }; 
 }
 
-console.log(' O Herói de nome ' + nome + ' está no nível de ' + nivel);
+
+vitorias = parseInt(prompt("Vitórias: "));
+derrotas = parseInt(prompt("Derrotas: "));
+
+resultado = calculoRank(vitorias, derrotas);
+
+console.log("O Herói tem um saldo de **"+resultado.saldoVitorias +"** e está no nível de **"+resultado.nivel+"**");
